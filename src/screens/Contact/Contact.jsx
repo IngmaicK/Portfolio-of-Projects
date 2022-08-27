23f1cf59d2746ad628b0bdd7e619404c1b289f08
Miguel <ingmaick@gmail.com>
2022-08-27 00:16:46 -0700

    Adding files to remote repository

---
+++ src/screens/Contact/Contact.jsx
@@ -0,0 +1,14 @@
+import "./Contact.css";
+
+function Contact() {
+  return (
+    <div id="contact" className="contact-container">
+      <p>Miguel Ramirez</p>
+      <p>Tijuana, BC</p>
+      <p>+52 (664) 252-2021</p>
+      <p>ingmaick@gmail.com</p>
+    </div>
+  )
+}
+
+export default Contact
